84b5dce2b9dbe7eaa01e3880948980c9082dfc27
Kattrina <kattrina.erillo@gmail.com>
2022-02-27 23:29:06 -0500

    set up backend

---
+++ server/express.js
@@ -0,0 +1,38 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
+import compress from 'compression'
+import cors from 'cors'
+import helmet from 'helmet'
+import Template from './../template'
+import userRoutes from './routes/user.routes'
+import authRoutes from './routes/auth.routes'
+
+const app = express()
+
+// parse body params and attache them to req.body
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cookieParser())
+app.use(compress())
+// secure apps by setting various HTTP headers
+app.use(helmet())
+// enable CORS - Cross Origin Resource Sharing
+app.use(cors())
+
+app.get('/', (req, res) => {
+  res.status(200).send(Template())
+})
+
+// mount routes
+app.use('/', userRoutes)
+app.use('/', authRoutes)
+
+// Catch unauthorised errors
+app.use((err, req, res, next) => {
+  if (err.name === 'UnauthorizedError') {
+    res.status(401).json({"error" : err.name + ": " + err.message})
+  }
+})
+
+export default app
